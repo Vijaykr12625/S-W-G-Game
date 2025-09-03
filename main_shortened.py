24903055e413c8c7e05cbd0a04fc21979bba96a2
Vijay Kumar Rana <vijaykumarrana.fac@gmail.com>
2025-09-04 03:54:32 +0530

    Add files via upload

---
+++ main_shortened.py
@@ -0,0 +1,42 @@
+import random
+'''
+1 for snake
+-1 for water
+0 for gun
+'''
+computer = random.choice([1,0,-1])
+youstr = input("Enter your choice : ")
+youdict = {"s": 1, "w": -1, "g": 0}
+reversedict = {1:"snake", -1:"water", 0:"gun"}
+
+you = youdict[youstr]
+
+# from here we have 2 numbers (variables), you & computer
+
+print(f"you chose {reversedict[you]}\ncomputer chose {reversedict[computer]}")
+
+if(computer == you):
+    print("It's draw")
+else:
+    '''
+    if(computer==1 and you==-1):  ---->computer-you = 2
+        print("You Loose!")
+    elif(computer==1 and you==0): ---->computer-you = 1
+        print("You Win!")
+    elif(computer==-1 and you==0): ---->computer-you = -1
+        print("You Loose!")
+    elif(computer==-1 and you==1): ---->computer-you = -2
+        print("You win!")
+    elif(computer==0 and you==-1): ---->computer-you = 1
+        print("You Win!")
+    elif(computer==0 and you==1):  ---->computer-you = -1
+        print("You Loose!")
+    else:
+        print("Somthing wrong!")
+    '''
+# This is summary of above code or conclusion in short form
+    if(computer-you==-1 or computer-you==2):
+        print("You Loose!")
+    else:
+        print("You Win!")
+    
